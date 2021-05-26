91aafd8242b7190f85ba5cfb61a91ea002d640bf
VinayTS <vinay@succorfish.com>
2021-05-26 14:21:39 +0530

    Resolved Splash screen Bug. a)White Screen comming before opening the splash screen.

--- Code/app/src/main/java/com/fullContact/Activities/SplashScreen.java
+++ Code/app/src/main/java/com/fullContact/Activities/SplashScreen.java
@@ -3,14 +3,21 @@ package com.fullContact.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.fullContact.R;
 
 public class SplashScreen extends AppCompatActivity {
-
+    TextView textView_tagline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        intializeViews();
+    }
+
+    private void intializeViews() {
+        textView_tagline=(TextView)findViewById(R.id.textView_tagline_id);
+        textView_tagline.setText("@ Finger Tip");
     }
 }
